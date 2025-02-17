32265eee39208fc51be0a99b497ebbec459cf413
user-425 <user425.itsme@gmail.com>
2025-02-17 11:10:42 +0700

    feat: add WelcomeController and update routes for hello and world

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,21 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'Selamat Datang';
-});
+Route::get('/', [PageController::class, 'index']);
 
-Route::get('/hello', function () {
-    return 'Hello World';
-});
-
-Route::get('/world', function () {
-    return 'World';
-});
-
-Route::get('/about', function () {
-    return '2341720105 - Achmad Anfasa Rabbany';
-});
+Route::get('/about', [PageController::class, 'about']);
 
 Route::get('/user/{name}', function ($name) {
     return 'Nama saya ' . $name;
@@ -37,11 +27,14 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
 });
 
-Route::get('/articles/{id}', function ($id) {
-    return 'Halaman artikel dengan ID ' . $id;
-});
+Route::get('/articles/{id}', [PageController::class, 'index']);
 
 Route::get('/user/{name?}', function ($name = 'John') {
     return 'Nama saya ' . $name;
 });
 
+Route::get('/hello', [WelcomeController::class, 'hello']);
+
+Route::get('/world', function () {
+    return 'World';
+});
