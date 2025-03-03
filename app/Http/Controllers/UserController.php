8c072a794666b70d180916dc358761178da95507
user-425 <user425.itsme@gmail.com>
2025-03-03 09:49:54 +0700

    feat: update UserController to use findOrFail for user retrieval (jobsheet 4, practicum 2.2)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -64,9 +64,22 @@ class UserController extends Controller
         // Jobsheet 4, Praktikum 2.1.10
         // ---------------------------
 
-         $user = UserModel::findOr(20, ['username' , 'nama'], function(){
-            abort(404);
-        });
+        //  $user = UserModel::findOr(20, ['username' , 'nama'], function(){
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
+
+        // Jobsheet 4, Praktikum 2.2.1
+        // ---------------------------
+
+        // $user = UserModel::findOrFail(1);
+        // return view('user', ['data' => $user]);
+
+        // Jobsheet 4, Praktikum 2.2.3
+        // ---------------------------
+
+        $user = UserModel::where('username','manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
+
