2a32a050dff551314ce9a4c4b7d864e05bbb9d0c
user-425 <user425.itsme@gmail.com>
2025-03-04 19:49:11 +0700

    feat: update UserController and user view to display user details instead of count (jobsheet 4, practicum 2.4.12)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -81,11 +81,51 @@ class UserController extends Controller
         // $user = UserModel::where('username','manager9')->firstOrFail();
         // return view('user', ['data' => $user]);
 
-         // Jobsheet 4, Praktikum 2.3.1
+        // Jobsheet 4, Praktikum 2.3.1
         // ---------------------------
-        $user = UserModel::where('level_id',2)->count();
+        // $user = UserModel::where('level_id',2)->count();
         // dd($user);
-        return view('user',['data' => $user]);
+        // return view('user',['data' => $user]);
+
+         // Jobsheet 4, Praktikum 2.4.1
+        // ---------------------------
+
+        // $user = UserModel::firstOrCreate([
+        //     'username' => 'manager',
+        //     'nama' => 'Manager'
+        // ]);
+        // return view('user', ['data' => $user]);
+
+        // Jobsheet 4, Praktikum 2.4.4
+        // ---------------------------
+
+        // $user = UserModel::firstOrCreate([
+        //     'username' => 'manager22',
+        //     'nama' => 'Manager Dua Dua',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2
+        // ]);
+        // return view('user', ['data' => $user]);
+
+        // Jobsheet 4, Praktikum 2.4.6
+        // ---------------------------
+
+        // $user = UserModel::firstOrNew([
+        //     'username' => 'manager',
+        //     'nama' => 'Manager',
+        // ]);
+        // return view('user', ['data' => $user]);
+
+        // Jobsheet 4, Praktikum 2.4.10
+        // ---------------------------
+
+        $user = UserModel::firstOrNew([
+            'username' => 'manager33',
+            'nama' => 'Manager Tiga Tiga',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
+        ]);
+        // $user->save();
+        return view('user', ['data' => $user]);
     }
 }
-
