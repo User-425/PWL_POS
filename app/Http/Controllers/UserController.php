82f43dd11869081434851cfd8e7e5bf9947da10f
user-425 <user425.itsme@gmail.com>
2025-03-04 20:07:35 +0700

    feat: update UserController to use isDirty, isClean, wasChanged to learn how they works (jobsheet 4, practicum 2.5)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -87,7 +87,7 @@ class UserController extends Controller
         // dd($user);
         // return view('user',['data' => $user]);
 
-         // Jobsheet 4, Praktikum 2.4.1
+        // Jobsheet 4, Praktikum 2.4.1
         // ---------------------------
 
         // $user = UserModel::firstOrCreate([
@@ -119,13 +119,59 @@ class UserController extends Controller
         // Jobsheet 4, Praktikum 2.4.10
         // ---------------------------
 
-        $user = UserModel::firstOrNew([
-            'username' => 'manager33',
-            'nama' => 'Manager Tiga Tiga',
-            'password' => Hash::make('12345'),
-            'level_id' => 2
-        ]);
+        // $user = UserModel::firstOrNew([
+        //     'username' => 'manager33',
+        //     'nama' => 'Manager Tiga Tiga',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2
+        // ]);
         // $user->save();
-        return view('user', ['data' => $user]);
+        // return view('user', ['data' => $user]);
+
+        // Jobsheet 4, Praktikum 2.5.1
+        // ---------------------------
+
+        // $user = UserModel::create([
+        //     'username' => 'manager55',
+        //     'nama' => 'Manager55',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2,
+        //     ]);
+        //     $user->username = 'manager56';
+
+        //     $user->isDirty(); // true
+        //     $user->isDirty('username'); // true
+        //     $user->isDirty('nama'); // false
+        //     $user->isDirty(['nama', 'username']); // true
+
+        //     $user->isclean(); // false
+        //     $user->isClean('username'); // false
+        //     $user->isClean('nama'); // true
+        //     $user->isClean(['nama', 'username']); // false
+
+        //     $user->save();
+
+        //     $user->isDirty(); // false
+        //     $user->isclean(); // true
+        //     dd($user->isDirty());
+
+        // Jobsheet 4, Praktikum 2.5.2
+        // ---------------------------
+
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+        $user->username = 'manager12';
+
+        $user->save();
+
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username', 'level_id']); // true
+        $user->wasChanged('nama'); // false
+        dd($user->wasChanged(['nama', 'username'])); // true
     }
 }
