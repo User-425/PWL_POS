9f26fb27491a87b1a5f314d1e8d679528b3fb8f8
user-425 <user425.itsme@gmail.com>
2025-03-04 22:32:38 +0700

    feat: add user management features with create, update, and delete functionality (jobsheet 4, practicum 2.6)

--- routes/web.php
+++ routes/web.php
@@ -67,4 +67,10 @@ Route::get('/greeting', [WelcomeController::class, 'greeting']);
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 
