62e43a405eadd02bdfc3432e97e1d78d0332a332
user-425 <user425.itsme@gmail.com>
2025-03-11 22:57:30 +0700

    feat: restructure routes for user, barang, and kategori management

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,10 @@
 <?php
 
-use App\Http\Controllers\AboutController;
-use App\Http\Controllers\ArticleController;
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
-use App\Http\Controllers\PageController;
-use App\Http\Controllers\PhotoController;
-use App\Http\Controllers\LevelController;
-use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\KategoriController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,53 +19,35 @@ use App\Http\Controllers\UserController;
 
 Route::get('/', [WelcomeController::class, 'index']);
 
-// Route::get('/about', [AboutController::class, 'index']);
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::post('/list', [UserController::class, 'list']);
+    Route::get('/create', [UserController::class, 'create']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::get('/{id}/edit', [UserController::class, 'edit']);
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']);
+});
 
-// Route::get('/user/{name}', function ($name) {
-//     return 'Nama saya ' . $name;
-// });
+Route::group(['prefix' => 'barang'], function () {
+    Route::get('/', [BarangController::class, 'index']);
+    Route::post('/list', [BarangController::class, 'list']);
+    Route::get('/create', [BarangController::class, 'create']);
+    Route::post('/', [BarangController::class, 'store']);
+    Route::get('/{id}', [BarangController::class, 'show']);
+    Route::get('/{id}/edit', [BarangController::class, 'edit']);
+    Route::put('/{id}', [BarangController::class, 'update']);
+    Route::delete('/{id}', [BarangController::class, 'destroy']);
+});
 
-// Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
-//     return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
-// });
-
-// Route::get('/articles/{id}', [ArticleController::class, 'index']);
-
-// Route::get('/user/{name?}', function ($name = 'John') {
-//     return 'Nama saya ' . $name;
-// });
-
-// Route::get('/hello', [WelcomeController::class, 'hello']);
-
-// Route::get('/world', function () {
-//     return 'World';
-// });
-
-// Route::resource('photos', PhotoController::class);
-
-// Include only specific methods (Whitelist method)
-// Route::resource('photos', PhotoController::class)->only([
-//     'index',
-//     'show'
-// ]);
-//
-// Exclude specific methods (Blacklist method)
-// Route::resource('photos', PhotoController::class)->except([
-//     'create',
-//     'store',
-//     'update',
-//     'destroy'
-// ]);
-
-Route::get('/greeting', [WelcomeController::class, 'greeting']);
-
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
-
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-
+Route::group(['prefix' => 'kategori'], function () {
+    Route::get('/', [KategoriController::class, 'index']);
+    Route::post('/list', [KategoriController::class, 'list']);
+    Route::get('/create', [KategoriController::class, 'create']);
+    Route::post('/', [KategoriController::class, 'store']);
+    Route::get('/{id}', [KategoriController::class, 'show']);
+    Route::get('/{id}/edit', [KategoriController::class, 'edit']);
+    Route::put('/{id}', [KategoriController::class, 'update']);
+    Route::delete('/{id}', [KategoriController::class, 'destroy']);
+});
