cda733a4d8cdca42b87fe9d26e2732503889e993
user-425 <user425.itsme@gmail.com>
2025-03-23 23:13:27 +0700

    feat: update authorization middleware to support multiple roles and add getRole method in UserModel

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::middleware(['auth'])->group(function () {
         Route::delete('/{id}', [UserController::class, 'destroy']);
     });
     
-    Route::middleware(['authorize:ADM'])->group(function () {
+    Route::middleware(['authorize:ADM,MNG'])->group(function () {
         Route::group(['prefix' => 'level'], function () {
             Route::get('/', [LevelController::class, 'index']);
             Route::post('/list', [LevelController::class, 'list']);
