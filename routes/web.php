05ecddc6a994a0bc0f3723dbd4be34f83ff5c9ae
user-425 <user425.itsme@gmail.com>
2025-04-14 09:00:06 +0700

    feat: add export functionality for User

--- routes/web.php
+++ routes/web.php
@@ -54,6 +54,7 @@ Route::middleware(['auth'])->group(function () {
             Route::delete('/{id}', [UserController::class, 'destroy']);
             Route::get('/import', [UserController::class, 'import']);
             Route::post('/import_ajax', [UserController::class, 'import_ajax']);
+            Route::get('/export_excel', [UserController::class, 'export_excel']);
         });
     });
 
